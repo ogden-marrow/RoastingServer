a59bb4622a9da7a944e3428048c7ffe07759c4f0
John Palenchar <jpalenchar.3@gmail.com>
2025-11-30 21:12:41 -0500

    Fresh Start

---
+++ RoastingServer.cpp
@@ -0,0 +1,12 @@
+﻿// RoastingServer.cpp : Defines the entry point for the application.
+//
+
+#include "RoastingServer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
